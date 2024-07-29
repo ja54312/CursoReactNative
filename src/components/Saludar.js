6370ed80c2b9931b77b193cb01b6dc87bd31f5c3
JA54312 <ija54312@gmail.com>
2024-07-29 14:22:51 -0600

    proptypes

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text } from "react-native";
+import PropTypes from "prop-types";
 
 export default function Saludar(props) {
   const { firstname, lastname } = props;
@@ -14,4 +15,9 @@ export default function Saludar(props) {
 Saludar.defaultProps = {
   firstname: "Jose",
   lastname: "Alatorre",
+};
+
+Saludar.prototypes = {
+  firstname: PropTypes.string.isRequired,
+  lastname: PropTypes.string.isRequired,
 };
