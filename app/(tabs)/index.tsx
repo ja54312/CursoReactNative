abc7f0e2d00fcb3c3d02be70adda5e1e0b87776a
JA54312 <ija54312@gmail.com>
2024-08-12 12:40:04 -0600

    navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import LoginForm from "../../src/components/LoginForm";
-import Saludar from "../../src/components/Saludar";
+
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Hola DEVICE</Text>
-      <Text>Hola Mundo</Text>
-      <LoginForm />
-      <Saludar firstname="Jose Antonio" lastname="Alatorre Chavez" />
+      <Text>Home</Text>
     </View>
   );
 }
